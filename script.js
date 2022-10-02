be0c502bbb71c853f98f509fb38268cee15256fa
Deivid Cuello <deividcuellocuevas@gmail.com>
2022-10-02 18:16:11 -0400

    updated

--- script.js
+++ script.js
@@ -1,62 +1,82 @@
-window.addEventListener('load', () => {
-	const taskForm = document.querySelector("#task-form");
-	const taskInput = document.querySelector("#task-input");
-	const tasks = document.querySelector("#tasks");
-    const taskCounter = document.querySelector("#task-counter");
+window.addEventListener("load", () => {
+  const taskForm = document.querySelector("#task-form");
+  const taskInput = document.querySelector("#task-input");
+  const tasks = document.querySelector("#tasks");
+  const taskCounter = document.querySelector("#task-counter");
+  const deleteAll = document.querySelector("#delete-all");
 
-	taskForm.addEventListener('submit', (e) => {
-		e.preventDefault();
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-        const newTask = taskInput.value;
+    const newTask = taskInput.value;
 
-        if(newTask != ''){
-            const task = document.createElement("div");
-            task.classList.add('task');
-    
-            const taskValue = document.createElement('input');
-            taskValue.type = 'text';
-            taskValue.value = newTask;
-            taskValue.setAttribute('readonly', 'readonly');
-            taskValue.autocomplete = 'of';
-            task.appendChild(taskValue);
-    
-            const taskBtn = document.createElement("div");
-            taskBtn.classList.add('task-btn');
-            const edtiBtn = document.createElement("button");
-            edtiBtn.innerText = 'Editar';
-            edtiBtn.classList.add('edit');
-            const deleteBtn = document.createElement("button");
-            deleteBtn.innerText = 'Eliminar'
-            deleteBtn.classList.add('delete');
-            taskBtn.appendChild(edtiBtn);
-            taskBtn.appendChild(deleteBtn);
-            task.appendChild(taskBtn)
-    
-            tasks.prepend(task);
+    if (newTask != "") {
+      const task = document.createElement("div");
+      task.classList.add("task");
 
-            taskCounter.innerText = `Tareas: ${tasks.childElementCount}`;
+      const taskValue = document.createElement("input");
+      taskValue.type = "text";
+      taskValue.value = newTask;
+      taskValue.setAttribute("readonly", "readonly");
+      taskValue.autocomplete = "of";
+      task.appendChild(taskValue);
 
-            taskInput.value = ''
-            edtiBtn.addEventListener('click', (e) => {
-                if (edtiBtn.innerText.toLowerCase() == "editar") {
-                    edtiBtn.innerText = "Guardar";
-                    taskValue.removeAttribute("readonly");
-                    taskValue.focus();
-                } else {
-                    edtiBtn.innerText = "Editar";
-                    taskValue.setAttribute("readonly", "readonly");
-                }
-            });
-            
-            deleteBtn.addEventListener('click', (e) => {
-                tasks.removeChild(task);
-                taskCounter.innerText = `Tareas: ${tasks.childElementCount}`;
-            });
+      const taskBtn = document.createElement("div");
+      taskBtn.classList.add("task-btn");
+      const edtiBtn = document.createElement("button");
+      edtiBtn.innerText = "Editar";
+      edtiBtn.classList.add("edit");
+      const deleteBtn = document.createElement("button");
+      deleteBtn.innerText = "Eliminar";
+      deleteBtn.classList.add("delete");
+      taskBtn.appendChild(edtiBtn);
+      taskBtn.appendChild(deleteBtn);
+      task.appendChild(taskBtn);
+
+      tasks.prepend(task);
+
+      taskCounter.innerText = `Tareas: ${tasks.childElementCount}`;
+
+      taskInput.value = "";
+      edtiBtn.addEventListener("click", (e) => {
+        if (edtiBtn.innerText.toLowerCase() == "editar") {
+          edtiBtn.innerText = "Guardar";
+          taskValue.removeAttribute("readonly");
+          taskValue.focus();
+        } else {
+          edtiBtn.innerText = "Editar";
+          taskValue.setAttribute("readonly", "readonly");
         }
+      });
 
+      deleteBtn.addEventListener("click", (e) => {
+        tasks.removeChild(task);
+        taskCounter.innerText = `Tareas: ${tasks.childElementCount}`;
+        if (tasks.childElementCount == 0) {
+          deleteAll.style.display = "none";
+        } else {
+          deleteAll.style.display = "inline";
+        }
+      });
 
+      if (tasks.childElementCount == 0) {
+        deleteAll.style.display = "none";
+      } else {
+        deleteAll.style.display = "inline";
+      }
+    }
+  });
 
+  if (tasks.childElementCount == 0) {
+    deleteAll.style.display = "none";
+  } else {
+    deleteAll.style.display = "inline";
+  }
 
+  deleteAll.addEventListener("click", (e) => {
+    while (tasks.firstChild) {
+        tasks.removeChild(tasks.firstChild);
+    }
 
-	});
+  });
 });
